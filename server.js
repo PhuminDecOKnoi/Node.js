cd650870196f31163f3f5bda20821823dd9e6daf
Phumin Decoknoi <139684701+PhuminDecOKnoi@users.noreply.github.com>
2023-07-16 18:26:56 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var url = require('url');
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: ""
+});
+conn.connect(function(err){
+    if(err) throw err;
+       console.log('Connected!');
+    
+    var sql = 'USE TEST';
+    conn.query(sql, function(err,result){
+        console.log("Result:" + result);
+    });
+
+    var sql = 'SELECT * FROM `tb_test` WHERE 1';
+    conn.query(sql, function(err, result){
+        http.createServer(function(req,res){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        var setStr = url.parse(req.url, true).query;
+        var getStr = setStr.dStr +" "+ setStr.mStr;
+        res.write("Result:" + result);
+        res.end();
+
+        }).listen(8888);
+    });
+});
