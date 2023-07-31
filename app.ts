25550b59ba90c333cd74bf295640b9776e71ec07
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-31 23:47:34 +0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,4 +1,10 @@
+const mysql = require('mysql')
 const myModule = require('./modules')
-console.log(myModule.myHelloworld())
-
-var mysq = require('mysql')
+const conn = myModule.connMySQL() //Connected to MySQL
+const sql : string = "SELECT * FROM `tb_test`"
+conn.query(sql,function(err:Boolean ,result : string[],fields : string[]){
+    if(err) throw err
+        console.log(result[0])
+        console.log(result[1])
+        console.log(result[2])
+     })
