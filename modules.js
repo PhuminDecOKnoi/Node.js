55dbe292c3d6787210e736e3041dfa4628a16b69
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-31 23:47:37 +0700

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,8 +1,19 @@
-exports.myHelloworld = function () {
-    var $message = "Hello world"
-    return $message
+var mysql = require('mysql');
+var HOSTNAME = 'localhost';
+var USER = 'root';
+var PWD = '';
+var DB = 'test';
+exports.connMySQL = function () {
+    var conn = mysql.createConnection({
+        host: HOSTNAME,
+        user: USER,
+        password: PWD,
+        database: DB
+    });
+    conn.connect(function (err) {
+        if (err)
+            throw err;
+        console.log("Connected!");
+    });
+    return conn;
 };
-
-exports.myConnMySQL =function() {
-    return
-}
